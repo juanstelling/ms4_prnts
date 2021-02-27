15650efe3d481ddde2f40bd29a4b1192b74d2f5e
juanstelling <juan.stelling@gmail.com>
2021-02-27 22:51:39 +0000

    Added products to home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from products.models import Product
+from django.core.paginator import Paginator
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+
+    context = {
+        'products': products,
+    }
+
+
+    return render(request, 'home/index.html', context)
