fc99dcb407a5567e8ffce3d17799cc37b028ec5a
juanstelling <juan.stelling@gmail.com>
2021-03-01 18:28:38 +0000

    added bag context and delivery logic

--- prnts/settings.py
+++ prnts/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 # Required by allauth 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -169,3 +170,6 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv("CLOUDINARY_API_SECRET")
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+FREE_DELIVERY_THRESHOLD = 45
+STANDARD_DELIVERY_PERCENTAGE = 10
