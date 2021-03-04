f6793d328edbe97cc2a14181b07df528d0cdecdb
juanstelling <juan.stelling@gmail.com>
2021-03-04 09:19:02 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IRCnqK3zhNmJSyO2dBAtpYh8HJEf5PJsWJIn8q4qnKCnwYjlsLZgnte2a6fPA3p98oKbFGlitn3ukM5tCrlwI3S00wKEetUvF',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
