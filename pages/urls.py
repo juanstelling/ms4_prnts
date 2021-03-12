dd2442be76e06c672d1b248acf601be18ce61f37
juanstelling <juan.stelling@gmail.com>
2021-03-12 13:57:57 +0000

    adjust main nav navigation

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('company/', views.company, name='company'),
-    path('lookbook/', views.lookbook, name='lookbook'),
     path('sustainability/', views.sustainability, name='sustainability'),
     path('faq/', views.faq, name='faq'),
     path('return-policy/', views.return_policy, name='return_policy'),
