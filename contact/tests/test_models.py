3b4fa0e34b0a3cabe5f4b46294d7a6debc33bee4
juanstelling <juan.stelling@gmail.com>
2021-03-14 18:48:54 +0000

    adjust testing for contact app

--- contact/tests/test_models.py
+++ contact/tests/test_models.py
@@ -5,11 +5,16 @@ from .models import ContactMessage
 class Test_Contact_Model(TestCase):
 
     def test_full_name_label(self):
+        # Get the contact object to test 
         contact = ContactMessage.objects.get(id=1)
+
+        # Get the metadata for the required field and use it to query the required field data
         field_label = contact._meta.get_field('full_name').verbose_name
+
+        # Compare the value to the expected result
         self.assertEqual(field_label, 'full_name')
 
-    def test_full_name(self):
+    def test_full_name_max_length(self):
         contact = ContactMessage.objects.get(id=1)
         max_length = contact._meta.get_field('full_name').max_length
         self.assertEqual(max_length, 50)
@@ -23,3 +28,7 @@ class Test_Contact_Model(TestCase):
         contact = ContactMessage.objects.get(id=1)
         field_label = contact._meta.get_field('message').verbose_name
         self.assertEqual(field_label, 'message')
+
+    def test_contact_str_method(self):
+        contact = ContactMessage.objects.create(name='test contact')
+        self.assertEqual(str(contact), 'test contact')
--- contact/tests/test_views.py
+++ contact/tests/test_views.py
@@ -3,8 +3,6 @@ from django.test import TestCase
 
 class TestContactViews(TestCase):
 
-    """ All contact view testing  """
-
     def test_contact_view(self):
         response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
