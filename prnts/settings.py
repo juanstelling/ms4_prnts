53b4591b158d05ae9f45eabc74079aa1c533574c
juanstelling <juan.stelling@gmail.com>
2021-03-16 15:38:05 +0000

    try again - deployment part 3 - adding aws for static files

--- prnts/settings.py
+++ prnts/settings.py
@@ -197,7 +197,7 @@ if 'USE_AWS' in os.environ:
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'prnts'
     AWS_S3_REGION_NAME = 'eu-west-3'
-    AWS_ACCES_KEY_ID = os.environ.get('AWS_ACCES_KEY_ID')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCES_KEY_ID')
     AWS_SECRET_ACCES_KEY = os.environ.get('AWS_SECRET_ACCES_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
