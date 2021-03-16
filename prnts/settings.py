64bbe7ef335591cf30a4b22ca0534d0e965f7aef
juanstelling <juan.stelling@gmail.com>
2021-03-16 14:30:24 +0000

    setup debug

--- prnts/settings.py
+++ prnts/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['prnts.herokuapp.com', 'localhost']
 
